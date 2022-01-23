69588eed8fa9088867cba561e5cec9efb05fed43
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-01-22 22:27:28 -0600

    Added Timer Reset and adding checkmarks after each repetition

--- main.py
+++ main.py
@@ -10,25 +10,61 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- #
+
+
+def reset_timer():
+    window.after_cancel(timer)
+    global reps
+    reps = 0
+    canvas.itemconfig(timer_text, text="00:00")
+    checkmarks_label.config(text="")
+    timer_label.config(text="Timer", fg=GREEN)
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 
-def start_timer(mins):
-    count_down(mins * 60)
+def start_timer():
+    global reps
+    reps += 1
+    work_time = WORK_MIN * 60
+    short_break = SHORT_BREAK_MIN * 60
+    long_break = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break)
+        timer_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break)
+        timer_label.config(text="Break", fg=PINK)
+    elif reps % 2 != 0:
+        count_down(work_time)
+        timer_label.config(text="Work", fg=GREEN)
+        window.attributes('-topmost', 1)
+        window.attributes('-topmost', 0)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
-
 def count_down(count):
-    count_min = math.floor(count / 60)
-    count_sec = count % 60
-    print(f"{count_min} : {count_sec}")
+    count_min = "{:02d}".format(math.floor(count / 60))
+    count_sec = "{:02d}".format(count % 60)
+    # print(f"{count_min} : {count_sec}")
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        for _ in range(math.floor(reps / 2)):
+            marks += "✔"
+
+        checkmarks_label.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -41,23 +77,21 @@ canvas.create_image(150, 130, image=tomato_img)
 timer_text = canvas.create_text(150, 150, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=2, row=2)
 
-# count_down(5)
-
 
 # TIMER LABEL
 timer_label = Label(text="Timer", font=(FONT_NAME, 50, "normal"), bg=YELLOW, fg=GREEN)
 timer_label.grid(column=2, row=1)
 
 # CHECKMARK LABELS
-checkmarks_label = Label(text="✔✔✔✔", font=(FONT_NAME, 20, "normal"), bg=YELLOW, fg=GREEN)
+checkmarks_label = Label(font=(FONT_NAME, 20, "normal"), bg=YELLOW, fg=GREEN)
 checkmarks_label.grid(column=2, row=4)
 
 # START BUTTON
-start_button = Button(text="Start", bg=YELLOW, highlightthickness=0, command=lambda: start_timer(WORK_MIN))
+start_button = Button(text="Start", bg=YELLOW, highlightthickness=0, command=lambda: start_timer())
 start_button.grid(column=1, row=3)
 
 # RESET BUTTON
-reset_button = Button(text="Reset", bg=YELLOW, highlightthickness=0)
+reset_button = Button(text="Reset", bg=YELLOW, highlightthickness=0, command=lambda: reset_timer())
 reset_button.grid(column=3, row=3)
 
 
